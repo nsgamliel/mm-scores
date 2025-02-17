3edeb5aa506759ec689d788cbf16a8cb58abdb2c
nsgamliel <nsgamliel@gmail.com>
2025-02-17 11:33:27 -0500

    ssl bug

--- scores-tracking.js
+++ scores-tracking.js
@@ -5,7 +5,8 @@ const pool = new Pool({
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  port: process.env.PG_PORT,
+  ssl: true
 });
 
 export const dropTables = async () => {
